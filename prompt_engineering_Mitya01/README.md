8d20242ff20cc97411bb15ec16cdce71ce926402
Mitya01 <dimitrimatyukov@yandex.ru>
2024-12-03 17:48:42 +0300

    Упаковка функции

---
+++ prompt_engineering_Mitya01/README.md
@@ -0,0 +1,3 @@
+# prompt-engineering
+
+A function that takes 2 arguments: credentials from GigaChat API and a job description and returns 5 screening questions.
---
+++ prompt_engineering_Mitya01/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Prompt_engineering',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/Mitya01/prompt-engineering',
+    author='Mitya01',
+    description='A function that takes 2 arguments: credentials from GigaChat API and a job description and returns 5 screening questions.'
+)
