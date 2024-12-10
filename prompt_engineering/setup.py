23f6c13bd06516d4b3f434ed8d233e8a7cba9435
Mitya01 <dimitrimatyukov@yandex.ru>
2024-12-10 16:44:42 +0300

    Изменение названия проекта

--- prompt_engineering/setup.py
+++ prompt_engineering/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prompt_engineering',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     url='https://github.com/Mitya01/prompt_engineering',
     author='Mitya01',
