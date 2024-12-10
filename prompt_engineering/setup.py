d2283393dc864640080f82ae5ef870535fcb6f5d
Mitya01 <dimitrimatyukov@yandex.ru>
2024-12-10 19:48:56 +0300

    Изменение пакета

--- prompt_engineering/setup.py
+++ prompt_engineering/setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='prompt_engineering',
+    name='gigachatprompt',
     version='1.1',
-    url='https://github.com/Mitya01/prompt_engineering',
     author='Mitya01',
     description='A function that takes 2 arguments: credentials from GigaChat API and a job description and returns 5 screening questions.'
 )
