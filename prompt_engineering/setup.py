b09f39348a48549503ee25bc88d3f6cc7ce91cfe
Mitya01 <dimitrimatyukov@yandex.ru>
2024-12-10 17:07:35 +0300

    Изменение пакета

--- prompt_engineering/setup.py
+++ prompt_engineering/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='prompt_engineering',
+    name='gigachat_prompt',
     version='1.1',
     packages=find_packages(),
     url='https://github.com/Mitya01/prompt_engineering',
