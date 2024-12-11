f55b691b33f72d6d3be2da1c2d58a9492c1e1394
Mitya01 <dimitrimatyukov@yandex.ru>
2024-12-11 19:30:24 +0300

    Изменение пакета и функции (добавление аргумента с названием вакансии)

--- prompt_engineering/setup.py
+++ prompt_engineering/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='gigachatprompt',
-    version='1.1',
+    version='1.2',
     author='Mitya01',
     description='A function that takes 2 arguments: credentials from GigaChat API and a job description and returns 5 screening questions.'
 )
